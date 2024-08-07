125cb8d9cce359ae5c1cf626e709e4f481a35fd0
Prashant Priyadarshi <48068632+PrashantUnity@users.noreply.github.com>
2024-08-07 22:45:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Statiq.App;
+using Statiq.Markdown;
+
+namespace Statiq
+{
+    internal class Program
+    {
+        public static async Task<int> Main(string[] args) => await Bootstrapper
+        .Factory
+        .CreateWeb(args)
+        .RunAsync();
+    }
+}
